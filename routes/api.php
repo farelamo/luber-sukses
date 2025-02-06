6c56c4b84d3fef0ef0ffb5eb620b35fff3b3c0ed
FarizFahmi <fariz.faturachmad@pcsindonesia.co.id>
2025-02-06 10:34:56 +0700

    adj: update routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\BrandController;;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\BrochureController;
 
 
 Route::post('login', [AuthController::class, 'login']);
@@ -12,5 +13,6 @@ Route::post('logout', [AuthController::class, 'logout']);
 
 Route::resource('product', ProductController::class);
 Route::resource('brand', BrandController::class);
+Route::resource('brochure', BrochureController::class);
 // Route::resource('career', CareerController::class);
 // Route::resource('service', ServiceController::class);
