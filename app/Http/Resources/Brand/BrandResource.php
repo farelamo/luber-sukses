5885cedf9206941785ec144ab633a334688442c3
FarizFahmi <fariz.faturachmad@pcsindonesia.co.id>
2025-02-06 10:38:22 +0700

    adj: update collection

--- app/Http/Resources/Brand/BrandResource.php
+++ app/Http/Resources/Brand/BrandResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Brand;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BrandResource extends JsonResource
--- app/Http/Resources/Brochure/BrochureResource.php
+++ app/Http/Resources/Brochure/BrochureResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Brochure;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BrochureResource extends JsonResource
