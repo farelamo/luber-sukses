560a2cf7233d9d53b9b57e2918a6477d6c58d9d5
FarizFahmi <fariz.faturachmad@pcsindonesia.co.id>
2025-02-06 23:44:59 +0700

    fix: fix typo

--- app/Http/Requests/BrochureRequest.php
+++ app/Http/Requests/BrochureRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class BrandRequest extends FormRequest
+class BrochureRequest extends FormRequest
 {
     public function authorize(): bool
     {
